a69251cb242f99c0742db082cf5162be959ff9ca
KlonZK <alexander.miethaner@t-online.de>
2015-07-20 11:15:40 +0200

    moved to git, added epic menu options

---
+++ LuaUI/Configs/ambient_emitters.lua
@@ -0,0 +1,228 @@
+--Emitters for positional sounds. Each sounditem will be unique for every emitter, if created by the widget.
+
+local Emitters = {
+	index = 1,
+	center = {
+		playlist = {			
+		},
+		pos = {
+			x = 4631,
+			y = 2496,
+			z = 3608,
+		},
+	},
+	center2 = {
+		playlist = {			
+		},
+		pos = {
+			x = 4688,
+			y = -4,
+			z = 3615,
+		},
+	},
+	center3 = {
+		playlist = {			
+		},
+		pos = {
+			x = 4786,
+			y = 1498,
+			z = 3622,
+		},
+	},
+	global = {
+		playlist = {},
+		pos = {
+			x = false,
+			y = false,
+			z = false,
+		},
+	},
+	ne1 = {
+		playlist = {},
+		pos = {
+			x = 5570,
+			y = 425,
+			z = 1026,
+		},
+	},
+	ne2 = {
+		playlist = {},
+		pos = {
+			x = 8862,
+			y = 371,
+			z = 342,
+		},
+	},
+	ne3 = {
+		playlist = {},
+		pos = {
+			x = 7867,
+			y = 466,
+			z = 3189,
+		},
+	},
+	ne4 = {
+		playlist = {},
+		pos = {
+			x = 7382,
+			y = 424,
+			z = 1806,
+		},
+	},
+	ne5 = {
+		playlist = {},
+		pos = {
+			x = 9191,
+			y = 21,
+			z = 2689,
+		},
+	},
+	ne6 = {
+		playlist = {},
+		pos = {
+			x = 9127,
+			y = 462,
+			z = 6534,
+		},
+	},
+	ne7 = {
+		playlist = {},
+		pos = {
+			x = 9206,
+			y = 195,
+			z = 5531,
+		},
+	},
+	rn1 = {
+		playlist = {},
+		pos = {
+			x = 3446,
+			y = -84,
+			z = 2826,
+		},
+	},
+	rn2 = {
+		playlist = {},
+		pos = {
+			x = 2987,
+			y = -56,
+			z = 2232,
+		},
+	},
+	rn3 = {
+		playlist = {},
+		pos = {
+			x = 2252,
+			y = -80,
+			z = 1592,
+		},
+	},
+	rn4 = {
+		playlist = {},
+		pos = {
+			x = 1771,
+			y = -71,
+			z = 893,
+		},
+	},
+	rs1 = {
+		playlist = {},
+		pos = {
+			x = 5682,
+			y = -80,
+			z = 4302,
+		},
+	},
+	rs2 = {
+		playlist = {},
+		pos = {
+			x = 6280,
+			y = -80,
+			z = 4983,
+		},
+	},
+	rs3 = {
+		playlist = {},
+		pos = {
+			x = 6901,
+			y = -79,
+			z = 5576,
+		},
+	},
+	rs4 = {
+		playlist = {},
+		pos = {
+			x = 7417,
+			y = -80,
+			z = 6288,
+		},
+	},
+	sw1 = {
+		playlist = {	
+			sw1_pigeon_1_mono = true,		
+		},
+		pos = {
+			x = 3604,
+			y = 386,
+			z = 5715,
+		},
+	},
+	sw2 = {
+		playlist = {},
+		pos = {
+			x = 760,
+			y = 376,
+			z = 6874,
+		},
+	},
+	sw3 = {
+		playlist = {},
+		pos = {
+			x = 2483,
+			y = 408,
+			z = 3364,
+		},
+	},
+	sw4 = {
+		playlist = {},
+		pos = {
+			x = 119,
+			y = 343,
+			z = 5195,
+		},
+	},
+	sw5 = {
+		playlist = {},
+		pos = {
+			x = 5,
+			y = 132,
+			z = 3143,
+		},
+	},
+	sw6 = {
+		playlist = {},
+		pos = {
+			x = 1361,
+			y = 449,
+			z = 1138,
+		},
+	},
+	sw7 = {
+		playlist = {},
+		pos = {
+			x = 1213,
+			y = 184,
+			z = 13,
+		},
+	},
+	test = {
+		playlist = {						
+		},
+		pos = {
+			x = 3197,
+			y = 1017,
+			z = 4358,
+		},
+	},
+}
+return Emitters
---
+++ LuaUI/Configs/ambient_options.lua
@@ -0,0 +1,18 @@
+--Config file. Words contains user-defined string variables
+
+local Options = {
+	config = {
+		ambientVolume = 2,
+		autoplay = true,
+		autoreload = true,
+		autosave = true,
+		path_map = "maps/white_rabbit_v30.sdd/",
+		path_read = "maps/white_rabbit_v30.sdd/Sounds/Ambient/",
+		path_sounds = "Sounds/Ambient/",
+		showEmitters = true,
+		verbose = true,
+	},
+	words = {
+	},
+}
+return Options
